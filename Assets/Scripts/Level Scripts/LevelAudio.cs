25d4a9488f6c3b0252b4e112cad1b826eb9e98f6
SirGamingBrain <chastan72@gmail.com>
2019-04-04 04:05:17 -0400

    Unity GitHub Beginning

---
+++ Assets/Scripts/Level Scripts/LevelAudio.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelAudio : MonoBehaviour
+{
+    float masterV = 0f;
+
+    public AudioSource audio;
+
+    public AudioClip levelMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio.loop = true;
+
+        audio.clip = levelMusic;
+
+        Debug.Log("Volume: " + PlayerPrefs.GetFloat("Master Volume"));
+
+        masterV = PlayerPrefs.GetFloat("Master Volume");
+
+        audio.volume = masterV * .8f;
+
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //masterV = PlayerPrefs.GetFloat("Master Volume");
+        audio.volume = masterV * .8f;
+    }
+}
